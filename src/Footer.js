1f59d2cc0056a8ccc2f464d2be8a18870d9181be
66MHassan <muhammadhassan662000@gmail.com>
2023-09-25 18:56:30 +0500

    Added footer component

---
+++ src/Footer.js
@@ -0,0 +1,31 @@
+export default function Footer (){
+    return(
+        <>
+        <footer>
+         <div className="footer">
+            <div className="container">
+               <div className="row">
+                  <div className="col-md-8 offset-md-2">
+                     <ul className="location_icon">
+                        <li><a href="#"><i className="fa fa-map-marker" aria-hidden="true"></i></a><br/> Location</li>
+                        <li><a href="#"><i className="fa fa-phone" aria-hidden="true"></i></a><br/> +92 3035174766</li>
+                        <li><a href="#"><i className="fa fa-envelope" aria-hidden="true"></i></a><br/> muhammadhassan@gmail.com</li>
+                     </ul>
+                  </div>
+               </div>
+            </div>
+            <div className="copyright">
+               <div className="container">
+                  <div className="row">
+                     <div className="col-md-12">
+                        <p><a href="https://html.design/"></a></p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </footer>
+        
+        </>
+    )
+}
